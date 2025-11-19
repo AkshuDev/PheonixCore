d16b186284b19d83cf92be53c093807f44b2d4e9
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2025-11-19 11:36:08 +0000

    Update with new funcs

--- pheonix_stdlib/pstdlib.h
+++ pheonix_stdlib/pstdlib.h
@@ -38,10 +38,12 @@ char* str = (char*)PNULL;
 #endif
 
 #if defined(__linux__)
-#include <sys/mman.h>
+    #include <sys/mman.h>
 #elif defined(_WIN32)
-#include <memoryapi.h>
-#include <winnt.h>
+    #include <memoryapi.h>
+    #include <winnt.h>
+    #include <windows.h>
+    #include <processthreadsapi.h>
 #endif
 
 #define __MEMTYPE_EXEC__ 1 // Memory Type: Executable
@@ -53,9 +55,11 @@ char* str = (char*)PNULL;
 #if defined(bool)
     #undef bool
 #endif
-#define bool _Bool; // Bool (0 = False, 1 = True)
+#define bool _Bool // Bool (0 = False, 1 = True)
 #define true ((bool)1)
 #define false ((bool)0)
+
+#define __PTR_ALIGNMENT 64 // Pointer Alignment (in bytes)
 
 // Typedefs
 typedef char byte_t; // Byte
@@ -121,14 +125,30 @@ struct PHM_Hdr {
     usize_t next_count;
 };
 
+/*
+PEHdlr: Pheonix Exit Handler
+*/
+struct PEHdlr {
+    void (*func)(void);
+    struct PEHdlr* next;
+};
+
+// Globals
+
+// - Static
+
+static struct PEHdlr* exit_handlers = NULL; // List of Exit functions
+
 // Functions
+
+// - Helpers
 
 /*
 Do a System Call
 
 Needs Arguments based on OS, all of size llong_t, if the Machine doesn't support 64-bit then the values will be truncated to fit
 */
-inline llong_t __plib_syscall(int id, ...) {
+inline llong_t syscall(int id, ...) {
     llong_t ret = 0;
     llong_t *args = (llong_t *)(&id + 1);
 
@@ -266,18 +286,56 @@ inline llong_t __plib_syscall(int id, ...) {
     return ret;
 }
 
+/*
+Align Pointer: Aligns a pointer 
+*/
+inline void* alignptr(void* ptr, usize_t alignment) {
+    uptr_t addr = (uptr_t)ptr;
+    return (void*)((addr + alignment - 1) & ~(alignment - 1));
+}
+
+/*
+Align Buffer: Aligns a buffer and returns a pointer to the aligned memory
+*/
+inline void* alignbuf(void* buf, usize_t alignment) {
+    return alignptr(buf, alignment);
+}
+
+/*
+Is Aligned: Checks if a pointer is already aligned to the given boundry
+*/
+inline bool isaligned(void* ptr, usize_t alignment) {
+    return ((uptr_t)ptr % alignment) == 0;
+}
+
+/*
+Align Up: Aligns a value up to the nearest multiple of alignment
+*/
+inline usize_t alignup(usize_t val, usize_t alignment) {
+    return (val + alignment - 1) & ~(alignment - 1);
+}
+
+/*
+Align Down: Aligns a value down to the nearest multiple of the alignment
+*/
+inline usize_t aligndown(usize_t val, usize_t alignment) {
+    return val & ~(alignment - 1);
+}
+
+// - Buffers/Memory
+
 /* Copy Buffer -
 Copy Memory from one place to another with specified size
 */
 inline bool copybuf(void* source, void* dest, usize_t size) {
     if (size == 0) return true;
-    else if (size < 0 || src == PNULL || dest == PNULL) return false;
+    else if (size < 0 || source == NULL || dest == NULL) return false;
 
     byte_t* dst = (byte_t*)dest;
     byte_t* src = (byte_t*)source;
 
     if (size < 32) {
-        for (usize_t i = 0; i < n; i++) dst[i] = src[i];
+        for (usize_t i = 0; i < size; i++) dst[i] = src[i];
         return true;
     }
 
@@ -285,7 +343,7 @@ inline bool copybuf(void* source, void* dest, usize_t size) {
     usize_t i = 0;
     if (p) {
         uptr_t to_align = (sizeof(u64) - p) & (sizeof(u64) - 1);
-        if (to_align > n) to_align = size;
+        if (to_align > size) to_align = size;
         for (; i < to_align; i++) dst[i] = src[i];
     }
 
@@ -293,7 +351,7 @@ inline bool copybuf(void* source, void* dest, usize_t size) {
     if (remain >= sizeof(u64)) {
         u64* wd = (u64*)(dst + i);
         u64* ws = (u64*)(src + i); 
-        usize words = remain / sizeof(u64);
+        usize_t words = remain / sizeof(u64);
         while (words >= 4) {
             wd[0] = ws[0];
             wd[1] = ws[1];
@@ -309,7 +367,7 @@ inline bool copybuf(void* source, void* dest, usize_t size) {
         i += ((size - i) / sizeof(u64)) * sizeof(u64);
     }
 
-    for(; i < n; i++) dst[i] = src[i];
+    for(; i < size; i++) dst[i] = src[i];
     return true;
 
 }
@@ -319,12 +377,12 @@ Fills Memory of specified size with the specified value
 */
 inline bool fillbuf(void* buf, byte_t value, usize_t size) {
     if (size == 0) return true;
-    else if (size < 0 || buf == PNULL) return false;
+    else if (size < 0 || buf == NULL) return false;
 
     byte_t* dest = (byte_t*)buf;
 
     if (size < 32) {
-        for (usize_t i = 0; i < n; i++) dest[i] = value;
+        for (usize_t i = 0; i < size; i++) dest[i] = value;
         return true;
     }
 
@@ -332,7 +390,7 @@ inline bool fillbuf(void* buf, byte_t value, usize_t size) {
     usize_t i = 0;
     if (p) {
         uptr_t to_align = (sizeof(u64) - p) & (sizeof(u64) - 1);
-        if (to_align > n) to_align = size;
+        if (to_align > size) to_align = size;
         for (; i < to_align; i++) dest[i] = value;
     }
 
@@ -340,7 +398,7 @@ inline bool fillbuf(void* buf, byte_t value, usize_t size) {
     if (remain >= sizeof(u64)) {
         u64 pat = (u64)value * (u64)0x0101010101010101ULL;
         u64 *wd = (u64*)(dest + i);
-        usize words = remain / sizeof(u64);
+        usize_t words = remain / sizeof(u64);
         while (words >= 4) {
             wd[0] = pat;
             wd[1] = pat;
@@ -355,7 +413,7 @@ inline bool fillbuf(void* buf, byte_t value, usize_t size) {
         i += ((size - i) / sizeof(u64)) * sizeof(u64);
     }
 
-    for(; i < n; i++) dest[i] = value;
+    for(; i < size; i++) dest[i] = value;
     return true;
 }
 
@@ -366,7 +424,6 @@ inline bool movebuf(void* source, void* dest, usize_t size) {
     if (!copybuf(source, dest, size)) return false;
     return fillbuf(source, 0, size);
 }
-
 
 /* Compare Buffer -
 Compare two blocks of Memory of the specified size
@@ -386,12 +443,14 @@ inline uoff_t findbyte(void* search_area, byte_t byte, usize_t size) {
     // Not Yet implemented
 }
 
+// - Allocation
+
 /*
 Extended Memory Alloc :
 Allocate Memory on the Heap, Can provide type of memory, such as Exec
 */
 inline void* exalloc(usize_t size, uint_t type, void* link) {
-    void* ptr = PNULL;
+    void* ptr = NULL;
     uint_t prot = 0;
 
     #if defined(__linux__)
@@ -399,10 +458,10 @@ inline void* exalloc(usize_t size, uint_t type, void* link) {
         if (type & __MEMTYPE_NONE__) prot |= PROT_NONE;
         if (type & __MEMTYPE_READ__) prot |= PROT_READ;
         if (type & __MEMTYPE_WRITE__) prot |= PROT_WRITE; 
-        ptr = mmap(PNULL, size + sizeof(PHM_Hdr), prot, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        ptr = mmap(NULL, size + sizeof(struct PHM_Hdr), prot, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
         if (ptr == MAP_FAILED) {
-            ptr = PNULL;
+            ptr = NULL;
         }
     #elif defined(_WIN32)
         if (type & __MEMTYPE_EXEC__ && type & __MEMTYPE_READ__ && type & __MEMTYPE_WRITE__) prot = PAGE_EXECUTE_READWRITE;
@@ -413,18 +472,18 @@ inline void* exalloc(usize_t size, uint_t type, void* link) {
         else if (type & __MEMTYPE_READ__) prot = PAGE_READONLY;
         else prot = PAGE_NOACCESS;
 
-        ptr = VirtualAlloc(PNULL, size + sizeof(PHM_Hdr), MEM_COMMIT | MEM_RESERVE, prot);
+        ptr = VirtualAlloc(NULL, size + sizeof(PHM_Hdr), MEM_COMMIT | MEM_RESERVE, prot);
     #else
-        ptr = PNULL;
+        ptr = NULL;
     #endif
 
     struct PHM_Hdr* hdr = (struct PHM_Hdr*)ptr;
     hdr->size = size;
     hdr->flags = 0; // Normal Allocated
-    hdr->next = link ? link - sizeof(PHM_Hdr) : PNULL;
+    hdr->next = link ? link - sizeof(struct PHM_Hdr) : NULL;
     if (link) hdr->next_count++;
 
-    return ptr + sizeof(PHM_Hdr);
+    return ptr + sizeof(struct PHM_Hdr);
 }
 
 /*
@@ -432,20 +491,20 @@ Memory Alloc -
 Allocate Memory on the heap with type Read/Write
 */
 inline void* alloc(usize_t size) {
-    return exalloc(size, __MEMTYPE_READ__ | __MEMTYPE_WRITE__, PNULL);
+    return exalloc(size, __MEMTYPE_READ__ | __MEMTYPE_WRITE__, NULL);
 }
 
 /* De Alloc -
 Deallocate the heap Allocated Memory
 */
 inline bool dealloc(void* ptr) {
-    if (ptr == PNULL) return false;
-    struct PHM_Hdr* hdr = ptr - sizeof(PHM_Hdr);
+    if (ptr == NULL) return false;
+    struct PHM_Hdr* hdr = ptr - sizeof(struct PHM_Hdr);
 
     if (hdr->next && hdr->next_count > 0) dealloc(hdr->next);
 
     if (hdr->flags != 0) return false; // DeAllocated Probably
-    usize_t size = hdr->size + sizeof(PHM_Hdr);
+    usize_t size = hdr->size + sizeof(struct PHM_Hdr);
     #if defined(__linux__)
         munmap((void*)hdr, size); // Hdr already points to the starting pos
     #elif defined(_WIN32)
@@ -462,9 +521,9 @@ Zeroed Memory Allocation -
 Allocate Zeroed Memory on the heap with type Read/Write
 */
 inline void* zalloc(usize_t size) {
-    void* ptr = exalloc(size, __MEMTYPE_READ__ | __MEMTYPE_WRITE__, PNULL);
-    if (ptr == PNULL) return ptr;
-    if (!fillbuf(ptr, 0, size)) { dealloc(ptr); return PNULL; }
+    void* ptr = exalloc(size, __MEMTYPE_READ__ | __MEMTYPE_WRITE__, NULL);
+    if (ptr == NULL) return ptr;
+    if (!fillbuf(ptr, 0, size)) { dealloc(ptr); return NULL; }
     return ptr;
 }
 
@@ -474,7 +533,7 @@ Reallocate Memory on the heap with type Read/Write
 */
 inline void* ralloc(void* ptr, usize_t size) {
     void* nptr = exalloc(size, __MEMTYPE_WRITE__ | __MEMTYPE_READ__, ptr);
-    if (nptr == PNULL) { dealloc(ptr); return PNULL; }
+    if (nptr == NULL) { dealloc(ptr); return NULL; }
     return ptr;
 }
 
@@ -484,8 +543,132 @@ Reallocate Zeroed Memory on the heap with type Read/Write
 */
 inline void* rzalloc(void* ptr, usize_t size) {
     void* nptr = exalloc(size, __MEMTYPE_READ__ | __MEMTYPE_WRITE__, ptr);
-    if (nptr == PNULL) { dealloc(ptr); return PNULL; }
-    if (!fillbuf(nptr, 0, size)) { dealloc(ptr); return PNULL; }
+    if (nptr == NULL) { dealloc(ptr); return NULL; }
+    if (!fillbuf(nptr, 0, size)) { dealloc(ptr); return NULL; }
     return ptr;
 }
 
+// - Strings
+
+/*
+String Length -
+Returns the length of a string (char*)
+*/
+inline usize_t strlen(const char* str) {
+    const char* s = str;
+    while (*s) s++;
+    return s - str;
+}
+
+/*
+String Copy -
+Copy a string from one place to another
+*/
+inline bool strcopy(const char* src, char* dest) {
+    return copybuf((void*)src, (void*)dest, strlen(src) + 1);
+}
+
+/*
+String Size Copy -
+Copy a string of the specified size from one place to another
+*/
+inline bool strscopy(const char* src, char* dest, usize_t size) {
+    return copybuf((void*)src, (void*)dest, size);
+}
+
+// - Environment
+
+/*
+Retrieve Environment -
+Retrieve and returns an environment variable
+*/
+inline char* retenv(const char* name) {
+    #if defined(__linux__)
+    #elif defined(_WIN32)
+    #endif
+    return NULL;
+}
+
+/*
+Set Environment -
+Set and returns an environment variable
+*/
+inline bool setenv(const char* name, const char* val, bool overwrite) {
+    #if defined(__linux__)
+    #elif defined(_WIN32)
+    #endif
+    return false;
+}
+
+// - General Utilities
+
+/*
+Append Exit Functions -
+Add a new entry to a list of functions that is executed upon exit (not abort)
+
+NOTE: These functions are executed from last appended to first appended
+*/
+inline bool aexitf(void (*func)(void)) {
+    if (func == NULL) return false;
+
+    struct PEHdlr* new_handler = (struct PEHdlr*)alloc(sizeof(struct PEHdlr));
+    if (new_handler == NULL) return false;
+
+    new_handler->func = func;
+    new_handler->next = exit_handlers;
+    exit_handlers = new_handler;
+
+    return true;
+}
+
+/*
+Exit -
+Exits the program gracefully, flushing everything properly
+*/
+inline void exit(int status) {
+    // Cleanup
+    // Exit Functions
+    struct PEHdlr* curef = exit_handlers;
+    while (curef != NULL) {
+        curef->func();
+        struct PEHdlr* temp = curef;
+        curef = curef->next;
+        dealloc(temp); // Free the handler
+    }
+
+    #if defined(__linux__)
+        // Calls exit_group
+        #if __PARCH__ == x86_64
+            syscall(231, (llong_t)status);
+        #elif __PARCH__ == x86
+            syscall(252, (llong_t)status);
+        #elif __PARCH__ == arm32
+            syscall(248, (llong_t)status);
+        #elif __PARCH__ == arm64
+            syscall(94, (llong_t)status);
+        #endif
+    #elif defined(_WIN32)
+        ExitProcess((unsigned int)status);
+    #endif
+}
+
+/*
+Abort -
+Immedietly Terminates the program with a core dump
+*/
+inline void abort(void) {
+    #if defined(__linux__)
+        // Calls exit_group with status 134 (128 + SIGABRT)
+        #if __PARCH__ == x86_64
+            syscall(231, 134);
+        #elif __PARCH__ == x86
+            syscall(252, 134);
+        #elif __PARCH__ == arm32
+            syscall(248, 134);
+        #elif __PARCH__ == arm64
+            syscall(94, 134);
+        #endif
+    #elif defined(_WIN32)
+        TerminateProcess(GetCurrentProcess(), 6);
+    #endif
+}
